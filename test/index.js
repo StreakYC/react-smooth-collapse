470bdac012ca6829c5e79ca34f868d20359c6a70
Chris Cowan <agentme49@gmail.com>
2017-04-25 11:37:39 -0700

    Require React ^0.14.9 || ^15.3.0.

--- test/index.js
+++ test/index.js
@@ -5,7 +5,7 @@ import assert from 'assert';
 import sinon from 'sinon';
 import React from 'react';
 import ReactDOM, {findDOMNode} from 'react-dom';
-import TestUtils from 'react-addons-test-utils';
+import TestUtils from 'react-dom/test-utils';
 import SmoothCollapse from '../src';
 
 describe('SmoothCollapse', function() {
