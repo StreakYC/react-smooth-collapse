d9ce4b2f544b5a4a5ba94c780401f348b5ef8e3b
Chris Cowan <agentme49@gmail.com>
2017-08-23 12:07:38 -0700

    Make test more jesty.

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,342 +1,310 @@
 /* @flow */
 /* eslint-disable react/no-find-dom-node */
 
-import assert from 'assert';
 import sinon from 'sinon';
 import React from 'react';
 import ReactDOM, {findDOMNode} from 'react-dom';
 import TestUtils from 'react-dom/test-utils';
 import SmoothCollapse from '../src';
 
-describe('SmoothCollapse', function() {
-
-  it('works after starting closed', function() {
-    const onChangeEnd = sinon.spy();
-
-    const div = document.createElement('div');
-
-    const root: SmoothCollapse = (ReactDOM.render(
-      <SmoothCollapse
-        expanded={false}
-        onChangeEnd={onChangeEnd}
-      >
-        <div className="foo">bar</div>
-        <div className="foo">more bar</div>
-      </SmoothCollapse>,
-      div
-    ): any);
-
-    const rootEl: any = findDOMNode(root);
-    sinon.spy(rootEl, 'addEventListener');
-    sinon.spy(rootEl, 'removeEventListener');
-
-    assert.strictEqual(root.state.height, '0');
-    assert.strictEqual(rootEl.style.transition, 'height .25s ease');
-    assert.strictEqual(rootEl.style.display, 'none');
-
-    assert.strictEqual(
-      TestUtils.scryRenderedDOMComponentsWithClass(root, 'foo').length,
-      0
-    );
-
-    assert(onChangeEnd.notCalled);
-
-    // EXPAND IT
-
-    ReactDOM.render(
-      <SmoothCollapse
-        expanded={true}
-        onChangeEnd={onChangeEnd}
-        heightTransition="4s ease-in-out 1s"
-      >
-        <div className="foo">bar</div>
-        <div className="foo">more bar</div>
-      </SmoothCollapse>,
-      div
-    );
-
-    assert.strictEqual(rootEl.style.transition, 'height 4s ease-in-out 1s');
-    assert.strictEqual(rootEl.style.display, '');
-
-    assert.strictEqual(
-      TestUtils.scryRenderedDOMComponentsWithClass(root, 'foo').length,
-      2
-    );
-
-    assert(onChangeEnd.notCalled);
-    assert(rootEl.addEventListener.calledOnce);
-    assert(rootEl.removeEventListener.notCalled);
-    assert.strictEqual(rootEl.addEventListener.args[0][0], 'transitionend');
-
-    rootEl.addEventListener.args[0][1](); //fake transitionend
-
-    assert(onChangeEnd.calledOnce);
-    assert(rootEl.addEventListener.calledOnce);
-    assert(rootEl.removeEventListener.calledOnce);
-    assert.strictEqual(rootEl.removeEventListener.args[0][0], 'transitionend');
-    assert.strictEqual(
-      rootEl.removeEventListener.args[0][1],
-      rootEl.addEventListener.args[0][1]
-    );
-
-    assert.strictEqual(root.state.height, 'auto');
-    assert.strictEqual(rootEl.style.display, '');
-
-    // CLOSE IT
-
-    ReactDOM.render(
-      <SmoothCollapse
-        expanded={false}
-        onChangeEnd={onChangeEnd}
-      >
-        <div className="foo">bar</div>
-        <div className="foo">more bar</div>
-      </SmoothCollapse>,
-      div
-    );
-
-    assert.strictEqual(root.state.height, '0');
-    assert.strictEqual(rootEl.style.display, '');
-
-    // Elements should still be there. If they were removed after being added
-    // before, then they could lose any state.
-    assert.strictEqual(
-      TestUtils.scryRenderedDOMComponentsWithClass(root, 'foo').length,
-      2
-    );
-
-    assert(onChangeEnd.calledOnce);
-    assert(rootEl.addEventListener.calledTwice);
-    assert(rootEl.removeEventListener.calledOnce);
-    assert.strictEqual(rootEl.addEventListener.args[1][0], 'transitionend');
-
-    rootEl.addEventListener.args[1][1](); //fake transitionend
-
-    assert.strictEqual(rootEl.style.display, 'none');
-
-    assert(onChangeEnd.calledTwice);
-    assert(rootEl.addEventListener.calledTwice);
-    assert(rootEl.removeEventListener.calledTwice);
-    assert.strictEqual(rootEl.removeEventListener.args[1][0], 'transitionend');
-    assert.strictEqual(
-      rootEl.removeEventListener.args[1][1],
-      rootEl.addEventListener.args[1][1]
-    );
-  });
-
-  it('works after starting opened', function() {
-    const onChangeEnd = sinon.spy();
-
-    const div = document.createElement('div');
-
-    const root: SmoothCollapse = (ReactDOM.render(
-      <SmoothCollapse
-        expanded={true}
-        onChangeEnd={onChangeEnd}
-      >
-        <div className="foo">bar</div>
-        <div className="foo">more bar</div>
-      </SmoothCollapse>,
-      div
-    ): any);
-
-    const rootEl: any = findDOMNode(root);
-    sinon.spy(rootEl, 'addEventListener');
-    sinon.spy(rootEl, 'removeEventListener');
-
-    assert.strictEqual(root.state.height, 'auto');
-    assert.strictEqual(rootEl.style.display, '');
-
-    assert.strictEqual(
-      TestUtils.scryRenderedDOMComponentsWithClass(root, 'foo').length,
-      2
-    );
-
-    assert(onChangeEnd.notCalled);
-
-    // CLOSE IT
-
-    ReactDOM.render(
-      <SmoothCollapse
-        expanded={false}
-        onChangeEnd={onChangeEnd}
-      >
-        <div className="foo">bar</div>
-        <div className="foo">more bar</div>
-      </SmoothCollapse>,
-      div
-    );
-
-    assert.strictEqual(root.state.height, '0');
-    assert.strictEqual(rootEl.style.display, '');
-
-    // Elements should still be there. If they were removed after being added
-    // before, then they could lose any state.
-    assert.strictEqual(
-      TestUtils.scryRenderedDOMComponentsWithClass(root, 'foo').length,
-      2
-    );
-
-    assert(onChangeEnd.notCalled);
-    assert(rootEl.addEventListener.calledOnce);
-    assert(rootEl.removeEventListener.notCalled);
-    assert.strictEqual(rootEl.addEventListener.args[0][0], 'transitionend');
-
-    rootEl.addEventListener.args[0][1](); //fake transitionend
-
-    assert.strictEqual(rootEl.style.display, 'none');
-
-    assert(onChangeEnd.calledOnce);
-    assert(rootEl.addEventListener.calledOnce);
-    assert(rootEl.removeEventListener.calledOnce);
-    assert.strictEqual(rootEl.removeEventListener.args[0][0], 'transitionend');
-    assert.strictEqual(
-      rootEl.removeEventListener.args[0][1],
-      rootEl.addEventListener.args[0][1]
-    );
-  });
-
-  it('works with collapse before expand finishes', function() {
-    const onChangeEnd = sinon.spy();
-
-    const div = document.createElement('div');
-
-    const root: SmoothCollapse = (ReactDOM.render(
-      <SmoothCollapse
-        expanded={false}
-        onChangeEnd={onChangeEnd}
-      >
-        <div className="foo">bar</div>
-        <div className="foo">more bar</div>
-      </SmoothCollapse>,
-      div
-    ): any);
-
-    const rootEl: any = findDOMNode(root);
-    sinon.spy(rootEl, 'addEventListener');
-    sinon.spy(rootEl, 'removeEventListener');
-
-    assert.strictEqual(root.state.height, '0');
-    assert.strictEqual(rootEl.style.display, 'none');
-
-    assert.strictEqual(
-      TestUtils.scryRenderedDOMComponentsWithClass(root, 'foo').length,
-      0
-    );
-
-    assert(onChangeEnd.notCalled);
-
-    // EXPAND IT
-
-    ReactDOM.render(
-      <SmoothCollapse
-        expanded={true}
-        onChangeEnd={onChangeEnd}
-      >
-        <div className="foo">bar</div>
-        <div className="foo">more bar</div>
-      </SmoothCollapse>,
-      div
-    );
-
-    assert.strictEqual(rootEl.style.display, '');
-
-    assert.strictEqual(
-      TestUtils.scryRenderedDOMComponentsWithClass(root, 'foo').length,
-      2
-    );
-
-    assert(onChangeEnd.notCalled);
-    assert(rootEl.addEventListener.calledOnce);
-    assert(rootEl.removeEventListener.notCalled);
-    assert.strictEqual(rootEl.addEventListener.args[0][0], 'transitionend');
-
-    // Don't signal transitionend yet
-
-    // CLOSE IT
-
-    ReactDOM.render(
-      <SmoothCollapse
-        expanded={false}
-        onChangeEnd={onChangeEnd}
-      >
-        <div className="foo">bar</div>
-        <div className="foo">more bar</div>
-      </SmoothCollapse>,
-      div
-    );
-
-    // Should have unsubscribed from waiting for the transitionend for the
-    // open.
-    assert(rootEl.removeEventListener.calledOnce);
-    assert.strictEqual(rootEl.removeEventListener.args[0][0], 'transitionend');
-    assert.strictEqual(
-      rootEl.removeEventListener.args[0][1],
-      rootEl.addEventListener.args[0][1]
-    );
-
-    assert.strictEqual(root.state.height, '0');
-    assert.strictEqual(rootEl.style.display, '');
-
-    // Elements should still be there. If they were removed after being added
-    // before, then they could lose any state.
-    assert.strictEqual(
-      TestUtils.scryRenderedDOMComponentsWithClass(root, 'foo').length,
-      2
-    );
-
-    assert(onChangeEnd.notCalled);
-    assert(rootEl.addEventListener.calledTwice);
-    assert(rootEl.removeEventListener.calledOnce);
-    assert.strictEqual(rootEl.addEventListener.args[1][0], 'transitionend');
-
-    rootEl.addEventListener.args[1][1](); //fake transitionend
-
-    assert.strictEqual(rootEl.style.display, 'none');
-
-    assert(onChangeEnd.calledOnce);
-    assert(rootEl.addEventListener.calledTwice);
-    assert(rootEl.removeEventListener.calledTwice);
-    assert.strictEqual(rootEl.removeEventListener.args[1][0], 'transitionend');
-    assert.strictEqual(
-      rootEl.removeEventListener.args[1][1],
-      rootEl.addEventListener.args[1][1]
-    );
-
-    // We're still closed, right?
-    assert.strictEqual(root.state.height, '0');
-  });
-
-  it('works after starting closed with non-zero collapsedHeight', function() {
-    const onChangeEnd = sinon.spy();
-
-    const div = document.createElement('div');
-
-    const root: SmoothCollapse = (ReactDOM.render(
-      <SmoothCollapse
-        expanded={false}
-        collapsedHeight="5px"
-        onChangeEnd={onChangeEnd}
-      >
-        <div className="foo">bar</div>
-        <div className="foo">more bar</div>
-      </SmoothCollapse>,
-      div
-    ): any);
-
-    const rootEl: any = findDOMNode(root);
-    sinon.spy(rootEl, 'addEventListener');
-    sinon.spy(rootEl, 'removeEventListener');
-
-    assert.strictEqual(root.state.height, '5px');
-    assert.strictEqual(rootEl.style.transition, 'height .25s ease');
-    assert.strictEqual(rootEl.style.display, '');
-
-    assert.strictEqual(
-      TestUtils.scryRenderedDOMComponentsWithClass(root, 'foo').length,
-      2
-    );
-
-    assert(onChangeEnd.notCalled);
-  });
+test('works after starting closed', () => {
+  const onChangeEnd = sinon.spy();
+
+  const div = document.createElement('div');
+
+  const root: SmoothCollapse = (ReactDOM.render(
+    <SmoothCollapse
+      expanded={false}
+      onChangeEnd={onChangeEnd}
+    >
+      <div className="foo">bar</div>
+      <div className="foo">more bar</div>
+    </SmoothCollapse>,
+    div
+  ): any);
+
+  const rootEl: any = findDOMNode(root);
+  sinon.spy(rootEl, 'addEventListener');
+  sinon.spy(rootEl, 'removeEventListener');
+
+  expect(root.state.height).toBe('0');
+  expect(rootEl.style.transition).toBe('height .25s ease');
+  expect(rootEl.style.display).toBe('none');
+
+  expect(TestUtils.scryRenderedDOMComponentsWithClass(root, 'foo').length)
+    .toBe(0);
+
+  expect(onChangeEnd.notCalled).toBe(true);
+
+  // EXPAND IT
+
+  ReactDOM.render(
+    <SmoothCollapse
+      expanded={true}
+      onChangeEnd={onChangeEnd}
+      heightTransition="4s ease-in-out 1s"
+    >
+      <div className="foo">bar</div>
+      <div className="foo">more bar</div>
+    </SmoothCollapse>,
+    div
+  );
+
+  expect(rootEl.style.transition).toBe('height 4s ease-in-out 1s');
+  expect(rootEl.style.display).toBe('');
+
+  expect(TestUtils.scryRenderedDOMComponentsWithClass(root, 'foo').length)
+    .toBe(2);
+
+  expect(onChangeEnd.notCalled).toBe(true);
+  expect(rootEl.addEventListener.calledOnce).toBe(true);
+  expect(rootEl.removeEventListener.notCalled).toBe(true);
+  expect(rootEl.addEventListener.args[0][0]).toBe('transitionend');
+
+  rootEl.addEventListener.args[0][1](); //fake transitionend
+
+  expect(onChangeEnd.calledOnce).toBe(true);
+  expect(rootEl.addEventListener.calledOnce).toBe(true);
+  expect(rootEl.removeEventListener.calledOnce).toBe(true);
+  expect(rootEl.removeEventListener.args[0][0]).toBe('transitionend');
+  expect(rootEl.removeEventListener.args[0][1])
+    .toBe(rootEl.addEventListener.args[0][1]);
+
+  expect(root.state.height).toBe('auto');
+  expect(rootEl.style.display).toBe('');
+
+  // CLOSE IT
+
+  ReactDOM.render(
+    <SmoothCollapse
+      expanded={false}
+      onChangeEnd={onChangeEnd}
+    >
+      <div className="foo">bar</div>
+      <div className="foo">more bar</div>
+    </SmoothCollapse>,
+    div
+  );
+
+  expect(root.state.height).toBe('0');
+  expect(rootEl.style.display).toBe('');
+
+  // Elements should still be there. If they were removed after being added
+  // before, then they could lose any state.
+  expect(TestUtils.scryRenderedDOMComponentsWithClass(root, 'foo').length)
+    .toBe(2);
+
+  expect(onChangeEnd.calledOnce).toBe(true);
+  expect(rootEl.addEventListener.calledTwice).toBe(true);
+  expect(rootEl.removeEventListener.calledOnce).toBe(true);
+  expect(rootEl.addEventListener.args[1][0]).toBe('transitionend');
+
+  rootEl.addEventListener.args[1][1](); //fake transitionend
+
+  expect(rootEl.style.display).toBe('none');
+
+  expect(onChangeEnd.calledTwice).toBe(true);
+  expect(rootEl.addEventListener.calledTwice).toBe(true);
+  expect(rootEl.removeEventListener.calledTwice).toBe(true);
+  expect(rootEl.removeEventListener.args[1][0]).toBe('transitionend');
+  expect(rootEl.removeEventListener.args[1][1])
+    .toBe(rootEl.addEventListener.args[1][1]);
+});
+
+test('works after starting opened', () => {
+  const onChangeEnd = sinon.spy();
+
+  const div = document.createElement('div');
+
+  const root: SmoothCollapse = (ReactDOM.render(
+    <SmoothCollapse
+      expanded={true}
+      onChangeEnd={onChangeEnd}
+    >
+      <div className="foo">bar</div>
+      <div className="foo">more bar</div>
+    </SmoothCollapse>,
+    div
+  ): any);
+
+  const rootEl: any = findDOMNode(root);
+  sinon.spy(rootEl, 'addEventListener');
+  sinon.spy(rootEl, 'removeEventListener');
+
+  expect(root.state.height).toBe('auto');
+  expect(rootEl.style.display).toBe('');
+
+  expect(TestUtils.scryRenderedDOMComponentsWithClass(root, 'foo').length)
+    .toBe(2);
+
+  expect(onChangeEnd.notCalled).toBe(true);
+
+  // CLOSE IT
+
+  ReactDOM.render(
+    <SmoothCollapse
+      expanded={false}
+      onChangeEnd={onChangeEnd}
+    >
+      <div className="foo">bar</div>
+      <div className="foo">more bar</div>
+    </SmoothCollapse>,
+    div
+  );
+
+  expect(root.state.height).toBe('0');
+  expect(rootEl.style.display).toBe('');
+
+  // Elements should still be there. If they were removed after being added
+  // before, then they could lose any state.
+  expect(TestUtils.scryRenderedDOMComponentsWithClass(root, 'foo').length)
+    .toBe(2);
+
+  expect(onChangeEnd.notCalled).toBe(true);
+  expect(rootEl.addEventListener.calledOnce).toBe(true);
+  expect(rootEl.removeEventListener.notCalled).toBe(true);
+  expect(rootEl.addEventListener.args[0][0]).toBe('transitionend');
+
+  rootEl.addEventListener.args[0][1](); //fake transitionend
+
+  expect(rootEl.style.display).toBe('none');
+
+  expect(onChangeEnd.calledOnce).toBe(true);
+  expect(rootEl.addEventListener.calledOnce).toBe(true);
+  expect(rootEl.removeEventListener.calledOnce).toBe(true);
+  expect(rootEl.removeEventListener.args[0][0]).toBe('transitionend');
+  expect(rootEl.removeEventListener.args[0][1])
+    .toBe(rootEl.addEventListener.args[0][1]);
+});
+
+it('works with collapse before expand finishes', function() {
+  const onChangeEnd = sinon.spy();
+
+  const div = document.createElement('div');
+
+  const root: SmoothCollapse = (ReactDOM.render(
+    <SmoothCollapse
+      expanded={false}
+      onChangeEnd={onChangeEnd}
+    >
+      <div className="foo">bar</div>
+      <div className="foo">more bar</div>
+    </SmoothCollapse>,
+    div
+  ): any);
+
+  const rootEl: any = findDOMNode(root);
+  sinon.spy(rootEl, 'addEventListener');
+  sinon.spy(rootEl, 'removeEventListener');
+
+  expect(root.state.height).toBe('0');
+  expect(rootEl.style.display).toBe('none');
+
+  expect(TestUtils.scryRenderedDOMComponentsWithClass(root, 'foo').length)
+    .toBe(0);
+
+  expect(onChangeEnd.notCalled).toBe(true);
+
+  // EXPAND IT
+
+  ReactDOM.render(
+    <SmoothCollapse
+      expanded={true}
+      onChangeEnd={onChangeEnd}
+    >
+      <div className="foo">bar</div>
+      <div className="foo">more bar</div>
+    </SmoothCollapse>,
+    div
+  );
+
+  expect(rootEl.style.display).toBe('');
+
+  expect(TestUtils.scryRenderedDOMComponentsWithClass(root, 'foo').length)
+    .toBe(2);
+
+  expect(onChangeEnd.notCalled).toBe(true);
+  expect(rootEl.addEventListener.calledOnce).toBe(true);
+  expect(rootEl.removeEventListener.notCalled).toBe(true);
+  expect(rootEl.addEventListener.args[0][0]).toBe('transitionend');
+
+  // Don't signal transitionend yet
+
+  // CLOSE IT
+
+  ReactDOM.render(
+    <SmoothCollapse
+      expanded={false}
+      onChangeEnd={onChangeEnd}
+    >
+      <div className="foo">bar</div>
+      <div className="foo">more bar</div>
+    </SmoothCollapse>,
+    div
+  );
+
+  // Should have unsubscribed from waiting for the transitionend for the
+  // open.
+  expect(rootEl.removeEventListener.calledOnce).toBe(true);
+  expect(rootEl.removeEventListener.args[0][0]).toBe('transitionend');
+  expect(rootEl.removeEventListener.args[0][1])
+    .toBe(rootEl.addEventListener.args[0][1]);
+
+  expect(root.state.height).toBe('0');
+  expect(rootEl.style.display).toBe('');
+
+  // Elements should still be there. If they were removed after being added
+  // before, then they could lose any state.
+  expect(TestUtils.scryRenderedDOMComponentsWithClass(root, 'foo').length)
+    .toBe(2);
+
+  expect(onChangeEnd.notCalled).toBe(true);
+  expect(rootEl.addEventListener.calledTwice).toBe(true);
+  expect(rootEl.removeEventListener.calledOnce).toBe(true);
+  expect(rootEl.addEventListener.args[1][0]).toBe('transitionend');
+
+  rootEl.addEventListener.args[1][1](); //fake transitionend
+
+  expect(rootEl.style.display).toBe('none');
+
+  expect(onChangeEnd.calledOnce).toBe(true);
+  expect(rootEl.addEventListener.calledTwice).toBe(true);
+  expect(rootEl.removeEventListener.calledTwice).toBe(true);
+  expect(rootEl.removeEventListener.args[1][0]).toBe('transitionend');
+  expect(rootEl.removeEventListener.args[1][1])
+    .toBe(rootEl.addEventListener.args[1][1]);
+
+  // We're still closed, right?
+  expect(root.state.height).toBe('0');
+});
+
+it('works after starting closed with non-zero collapsedHeight', function() {
+  const onChangeEnd = sinon.spy();
+
+  const div = document.createElement('div');
+
+  const root: SmoothCollapse = (ReactDOM.render(
+    <SmoothCollapse
+      expanded={false}
+      collapsedHeight="5px"
+      onChangeEnd={onChangeEnd}
+    >
+      <div className="foo">bar</div>
+      <div className="foo">more bar</div>
+    </SmoothCollapse>,
+    div
+  ): any);
+
+  const rootEl: any = findDOMNode(root);
+  sinon.spy(rootEl, 'addEventListener');
+  sinon.spy(rootEl, 'removeEventListener');
+
+  expect(root.state.height).toBe('5px');
+  expect(rootEl.style.transition).toBe('height .25s ease');
+  expect(rootEl.style.display).toBe('');
+
+  expect(TestUtils.scryRenderedDOMComponentsWithClass(root, 'foo').length)
+    .toBe(2);
+
+  expect(onChangeEnd.notCalled).toBe(true);
 });
